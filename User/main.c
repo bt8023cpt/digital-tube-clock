954fc91039b78208c49eaa58e70e3bb13ab9e69c
bt8023cpt_windows_7 <bt8023cpt@163.com>
2017-01-25 00:03:23 +0800

    feature: realize port of view code 1.defines the enumeration variable indicate electronic state 2.realize the ds1302 view code

--- User/main.c
+++ User/main.c
@@ -8,12 +8,18 @@
 #define Segment P0
 #define Digital P2
 
+sbit beep = P3^6;
+
 /* 定义模型变量 */
 u8 location = 0;								// 记录当期是哪一位闪烁
 u8 value[4] = {1, 2, 3, 4};							// 记录当前数码管显示的值
+u8 alarm[4] = {1, 2, 3, 4};							// 记录
 
-/* 共阴极数码管段选码、位选码 */
-u8 SegCode[16] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
+enum display_mode state = WeekDisplay;
+enum flicker_state flag = flicker;
+
+/* 共阴极数码管段选码、位选码 '0123456789abcdef-' */
+u8 SegCode[17] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x40};
 u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};
 
 int main()
@@ -27,18 +33,120 @@ int main()
         IT0 = 1;
         IE = 0x81;
         
-        DS1302_WriteRegister(0x8E,0X00);
+        DS1302_WriteRegister(DS1302_CONTROL | DS1302_WRITE, 0x00);
         
         while(1)
         {
-                tmp1 = DS1302_ReadRegister(DS1302_MIN | DS1302_READ);
-                tmp2 = DS1302_ReadRegister(DS1302_SEC | DS1302_READ);
+                switch(state)
+                {
+                        case (ClockDisplay):
+                                tmp1 = DS1302_ReadRegister(DS1302_HR | DS1302_READ);
+                                tmp2 = DS1302_ReadRegister(DS1302_MIN | DS1302_READ);
+                                flag = light;
+                                
+                                value[0] = (tmp1 & 0xf0) >> 4;
+                                value[1] = tmp1 & 0x0f;
+                                value[2] = (tmp2 & 0xf0) >> 4;
+                                value[3] = tmp2 & 0x0f;
+                        
+                                if((value[0] == alarm[0]) && (value[1] == alarm[1]) && (value[2] == alarm[2]) && (value[3] == alarm[3]))
+                                {
+                                        beep = 1;
+                                }
+                                else
+                                {
+                                        beep = 0;
+                                }
+                                break;
+                        
+                        case (ClockSet):
+                                tmp1 = DS1302_ReadRegister(DS1302_HR | DS1302_READ);
+                                tmp2 = DS1302_ReadRegister(DS1302_MIN | DS1302_READ);
+                                flag = flicker;
+                        
+                                value[0] = (tmp1 & 0xf0) >> 4;
+                                value[1] = tmp1 & 0x0f;
+                                value[2] = (tmp2 & 0xf0) >> 4;
+                                value[3] = tmp2 & 0x0f;
+                                break;
+                        
+                        case (DateDisplay):
+                                tmp1 = DS1302_ReadRegister(DS1302_MONTH | DS1302_READ);
+                                tmp2 = DS1302_ReadRegister(DS1302_DATE | DS1302_READ);
+                                flag = light;
+                        
+                                value[0] = (tmp1 & 0xf0) >> 4;
+                                value[1] = tmp1 & 0x0f;
+                                value[2] = (tmp2 & 0xf0) >> 4;
+                                value[3] = tmp2 & 0x0f;
+                                break;
+                        
+                        case (DateSet):
+                                tmp1 = DS1302_ReadRegister(DS1302_MONTH | DS1302_READ);
+                                tmp2 = DS1302_ReadRegister(DS1302_DATE | DS1302_READ);
+                        
+                                value[0] = (tmp1 & 0xf0) >> 4;
+                                value[1] = tmp1 & 0x0f;
+                                value[2] = (tmp2 & 0xf0) >> 4;
+                                value[3] = tmp2 & 0x0f;
+                                break;
+                        
+                        case (YearDisplay):
+                                tmp1 = DS1302_ReadRegister(DS1302_YEAR | DS1302_READ);
+                                flag = light;
+                        
+                                value[0] = 2;
+                                value[1] = 0;
+                                value[2] = (tmp1 & 0xf0) >> 4;
+                                value[3] = tmp1 & 0x0f;
+                                break;
+                        
+                        case (YearSet):
+                                tmp1 = DS1302_ReadRegister(DS1302_YEAR | DS1302_READ);
+                                flag = flicker;
+                        
+                                value[0] = 2;
+                                value[1] = 0;
+                                value[2] = (tmp1 & 0xf0) >> 4;
+                                value[3] = tmp1 & 0x0f;
+                                break;
+                        
+                        case (WeekDisplay):
+                                tmp1 = DS1302_ReadRegister(DS1302_DAY | DS1302_READ);
+                                flag = light;
+                        
+                                value[0] = 16;
+                                value[1] = 16;
+                                value[2] = tmp1 & 0x0f;
+                                value[3] = 16;
+                                break;
+                        
+                        case (WeekSet):
+                                tmp1 = DS1302_ReadRegister(DS1302_DAY | DS1302_READ);
+                                flag = flicker;
+                        
+                                value[0] = 16;
+                                value[1] = 16;
+                                value[2] = tmp1 & 0x0f;
+                                value[3] = 16;
+                                break;
+                        
+                        case (AlarmDisplay):
+                                flag = light;
+                                break;
+                        
+                        case (AlarmSet):
+                                flag = flicker;
+                                break;
+                        
+                        case (TemperatureDisplay):
+                                flag = light;
+                                break;
 
-                value[0] = (tmp1 & 0xf0) >> 4;
-                value[1] = tmp1 & 0x0f;
+                }
                 
-                value[2] = (tmp2 & 0xf0) >> 4;
-                value[3] = tmp2 & 0x0f;
+                
+                
                 
                 /* 这里扫描的时候是正常显示4位 */
                 for(j = flickerCount; j > 0; j--)
@@ -60,37 +168,39 @@ int main()
                                 delay_us(50);
                         }
                 }
-                
-                /* 这里扫描的时候location对应位不显示，只显示其他位 */
-                for(j = flickerCount; j > 0; j--)
+                        
+                if(flicker == flag)
                 {
-                        for(i = 0; i < 4; i++)
+                        /* 这里扫描的时候location对应位不显示，只显示其他位 */
+                        for(j = flickerCount; j > 0; j--)
                         {
-                                /* 判断当期扫描的位是不是location对应位 */
-                                if(location == i)
+                                for(i = 0; i < 4; i++)
                                 {
-                                        /* 如果是的话，不选中任何位选，也就是全灭 */
-                                        Digital = 0xff;
+                                        /* 判断当期扫描的位是不是location对应位 */
+                                        if(location == i)
+                                        {
+                                                /* 如果是的话，不选中任何位选，也就是全灭 */
+                                                Digital = 0xff;
+                                        }
+                                        else
+                                        {
+                                                /* 对于其他为就正常显示就好 */
+                                                Digital = DigCode[i];
+                                        }
+                                        
+                                        /* 这里和上面是一样的，加小数点的操作 */
+                                        if(i == 1)
+                                        {
+                                                Segment = SegCode[value[i]] | 0x80;
+                                        }
+                                        else
+                                        {
+                                                Segment = SegCode[value[i]];
+                                        }
+                                        delay_us(50);
                                 }
-                                else
-                                {
-                                        /* 对于其他为就正常显示就好 */
-                                        Digital = DigCode[i];
-                                }
-                                
-                                /* 这里和上面是一样的，加小数点的操作 */
-                                if(i == 1)
-                                {
-                                        Segment = SegCode[value[i]] | 0x80;
-                                }
-                                else
-                                {
-                                        Segment = SegCode[value[i]];
-                                }
-                                delay_us(50);
                         }
                 }
-                
         }
 }
 
--- User/main.h
+++ User/main.h
@@ -5,5 +5,26 @@
 #define u8 unsigned char
 #define u16 unsigned int
 #define u32 unsigned long
+        
+enum display_mode
+{
+        ClockDisplay = 0,
+        ClockSet = 1,
+        DateDisplay = 2,
+        DateSet = 3,
+        YearDisplay = 4,
+        YearSet = 5,
+        WeekDisplay = 6,
+        WeekSet = 7,
+        AlarmDisplay = 8,
+        AlarmSet = 9,
+        TemperatureDisplay = 10
+};
+
+enum flicker_state
+{
+        light = 0,
+        flicker = 1
+};
 
 #endif /* __MAIN_H */
